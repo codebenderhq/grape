c4d77cecf0846a7e023716e0e856d27076aba8bf
rawkakani <lionelmbankeu@gmail.com>
2022-12-20 10:00:49 +0200

    added html support

--- src/_server/.middleware/html.js
+++ src/_server/.middleware/html.js
@@ -1,38 +1,71 @@
 import { exists } from "https://deno.land/std/fs/mod.ts";
 
+const exts = ['html','jsx'] 
+let isError = false
+let _path = './src/_app'
+const errorPath = `${_path}/error/pages/index.html`
 
-const html_middleware = async (pathname, req, path = './src/_app') => {
-  
 
-  let pageSrc = `${path}/index.html`
-  let paramPage = null
+const html_middleware = async (pathname, req, path = _path) => {
+
+  let paramPage = ''
+  let jsxPage = false
+  let page;
+
+  if(path !== "error"){
+    for (const ext of exts) {
+      let _pageSrc = `${path}/index.${ext}`
   
-  if(pathname.split('/').length === 2 && pathname !== '/'){
-    console.log('htllo')
-    pageSrc = `${path}/${pathname}/pages/index.html`
-  } else if(pathname !== '/'){ 
-    pageSrc = `${path}/${pathname.split('/')[1]}/pages/${pathname.split('/')[2]}.html` 
-    paramPage = `${path}/${pathname.split('/')[1]}/pages/@.html`;
+      if(pathname.split('/').length === 2 && pathname !== '/'){
+        _pageSrc = `${path}/${pathname}/pages/index.${ext}`;
+      } else if(pathname !== '/'){ 
+        _pageSrc = `${path}/${pathname.split('/')[1]}/pages/${pathname.split('/')[2]}.${ext}` 
+        paramPage = `${path}/${pathname.split('/')[1]}/pages/@.${ext}`;
+      }
+      
+  
+      const isParamAvailible = await exists(paramPage)
+      const pageExist = await exists(_pageSrc)
+  
+      // console.log(pageExist, isParamAvailible)
+  
+      if(!page && ext !== 'jsx'){
+        page = await Deno.readFile(pageExist ? _pageSrc : isParamAvailible ? paramPage : set_error() );
+  
+        // until a better soultion is found
+        if(pathname === '/'){
+          break;
+        }
+      }
+   
+      if(ext === 'jsx' && isError && pageExist || isParamAvailible){
+        jsxPage = await import(`../../../${pageExist ? _pageSrc : isParamAvailible ? paramPage : set_error()}`)
+      }
+    }
+  
+    if(jsxPage){
+      return jsxPage.default()
+    }
+  }else{
+   return error_response()
   }
+
   
- 
-  if(!await exists(pageSrc) && !await exists(paramPage)){
-    pageSrc = `${path}/error/pages/index.html`
-  } 
- 
-  console.log(Deno.cwd())
-  console.log(pageSrc)
- 
-  const page = await Deno.readFile(pageSrc);
-  const res = page ? page : paramPage ? paramPage : errorPage
 
-  if(await exists(`../../_app/${pathname}.jsx`)){
-     
-    const jsxResponse =  await import(`../src/_server/public/${res.page}`) 
-    return jsxResponse.default()
- 
-  }
+  return html_response(page)
+}
 
+const set_error = () => {
+  isError = true
+  return errorPath
+
+}
+
+export const error_response = () => {
+  return html_response(Deno.readFile(errorPath))
+}
+
+const html_response = (res) => {
   return new Response(res, {
     headers: {
       "content-type": "text/html",
